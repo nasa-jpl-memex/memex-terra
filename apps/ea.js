6299c6f8c58209978bb6b6e50863122c9e95389c
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2015-05-22 13:39:23 -0400

    Added support for multiple time-series

--- apps/ea.js
+++ apps/ea.js
@@ -6,16 +6,20 @@ eaApp.getTimeSeries = function() {
   $.ajax({
     url: "http://cors-anywhere.herokuapp.com/http://tempus-demo.ngrok.com/api/series?table=escort_ads&sort=1&response_col=price_per_hour&group_col=msaname&group=Memphis,%20TN-AR-MS%20MSA",
     // Work with the response
-    success: function( response ) {
-        console.log( response ); // server response
-        eaApp.timeSeries(response);
+    success: function( data ) {
+        $.ajax({
+            url: "http://cors-anywhere.herokuapp.com/http://tempus-demo.ngrok.com/api/comparison?table=escort_ads&sort=1&response_col=price_per_hour&group_col=msaname&group=Bakersfield,%20CA%20MSA&covs=population%7Cviolent",
+            success: function( compData ) {
+                eaApp.timeSeries( data, compData );
+            }
+        })
     }
   });
 }
 
 // Create statistical plot
 //--------------------------------------------------------------------------
-eaApp.timeSeries = function(data, clearPrev) {
+eaApp.timeSeries = function(data, compData, clearPrev) {
   // var data = [
   //   {"date": "2012-01-05",  "value": 28},
   //   {"date": "2012-01-10",  "value": 43}
@@ -25,8 +29,15 @@ eaApp.timeSeries = function(data, clearPrev) {
   // Prepare data
   data = JSON.parse(data);
   for (i = 0; i < data.result.length; ++i) {
-    cleanData.push({"date": data.result[i][0], "value": data.result[i][1]});
+    cleanData.push({"date": data.result[i][0], "value": data.result[i][1], "symbol": "raw"});
   }
+
+  data = JSON.parse(compData);
+  for (i = 0; i < data.result.length; ++i) {
+    cleanData.push({"date": data.result[i][0], "value": data.result[i][1], "symbol": "comp"});
+  }
+
+  console.log(cleanData);
 
   var spec = {
     "width": $("#statistics").width() * 0.90,
@@ -55,9 +66,7 @@ eaApp.timeSeries = function(data, clearPrev) {
         "domain": {"data": "table", "field": "data.value"}
       },
       {
-        "name": "color",
-        "type": "ordinal",
-        "range": "category20"
+        "name": "color", "type": "ordinal", "range": "category10"
       }
     ],
     "axes": [
@@ -66,50 +75,72 @@ eaApp.timeSeries = function(data, clearPrev) {
     ],
     "marks": [
       {
-        "type": "line",
-        "from": {"data": "table"},
-        "properties": {
-          "enter": {
-            "interpolate": {"value": "monotone"},
-            "x": {"scale": "x", "field": "data.date"},
-            "y": {"scale": "y", "field": "data.value"},
-            "size": {"value": 50},
-            "stroke": {"scale": "color", "field": "data.indexname"},
-            "strokeWidth": {"value": 2}
+        "type": "group",
+        "from": {
+          "data": "table",
+          "transform": [{"type": "facet", "keys": ["data.symbol"]}]
+        },
+        "marks": [
+          {
+            "type": "line",
+            "properties": {
+              "enter": {
+                "interpolate": {"value": "monotone"},
+                "x": {"scale": "x", "field": "data.date"},
+                "y": {"scale": "y", "field": "data.value"},
+                "size": {"value": 50},
+                "stroke": {"scale": "color", "field": "data.symbol"},
+                "strokeWidth": {"value": 2}
+              },
+              "update": {
+                "opacity": {"value": 1}
+              },
+              "hover": {
+                "opacity": {"value": 0.5}
+              }
+            }
           },
-          "update": {
-            "opacity": {"value": 1}
+          {
+            "type": "symbol",
+            "from": {"data": "table"},
+            "properties": {
+              "enter": {
+                "interpolate": {"value": "monotone"},
+                "x": {"scale": "x", "field": "data.date"},
+                "y": {"scale": "y", "field": "data.value"},
+                "size": {"value": 50},
+                "fill": {
+                    "r": {"value": 255},
+                    "g": {"value": 100},
+                    "b": {"value": 100}
+                },
+                "strokeWidth": {"value": 2}
+              },
+              "update": {
+                "opacity": {"value": 1}
+              },
+              "hover": {
+                "opacity": {"value": 0.5}
+              }
+            }
           },
-          "hover": {
-            "opacity": {"value": 0.5}
-          }
-        }
-      },
-      {
-        "type": "symbol",
-        "from": {"data": "table"},
-        "properties": {
-          "enter": {
-            "interpolate": {"value": "monotone"},
-            "x": {"scale": "x", "field": "data.date"},
-            "y": {"scale": "y", "field": "data.value"},
-            "size": {"value": 50},
-            "fill": {
-                "r": {"value": 255},
-                "g": {"value": 100},
-                "b": {"value": 100}
+          {
+            "type": "text",
+            "from": {
+              "transform": [{"type": "filter", "test": "index==data.length-1"}]
             },
-            "strokeWidth": {"value": 2}
-          },
-          "update": {
-            "opacity": {"value": 1}
-          },
-          "hover": {
-            "opacity": {"value": 0.5}
+            "properties": {
+              "enter": {
+                "x": {"scale": "x", "field": "data.date", "offset": 40},
+                "y": {"scale": "y", "field": "data.value"},
+                "fill": {"scale": "color", "field": "data.symbol"},
+                "text": {"field": "data.symbol"},
+                "baseline": {"value": "middle"}
+               }
+            }
           }
-        }
+        ]
       }
-
     ]
   }
 
