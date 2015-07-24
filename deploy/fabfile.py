d4496b1f8b374a3d9b0b1e56950d722ad7bf4a4b
Dan LaManna <dan.lamanna@kitware.com>
2015-07-24 10:34:25 -0400

    Updates docker image tags, add fabric deploy script

---
+++ deploy/fabfile.py
@@ -0,0 +1,26 @@
+from fabric.api import sudo, run
+from fabric.contrib.files import exists
+from fabric.decorators import task
+
+crane_file = 'https://raw.githubusercontent.com/Kitware/SMQTK/tempus/python/smqtk/web/geospace/deploy/crane.yaml'
+
+
+def install_crane():
+    if not exists('crane'):
+        run('bash -c "`curl -sL https://raw.githubusercontent.com/michaelsauter/crane/master/download.sh`"')
+
+
+@task
+def deploy():
+    install_crane()
+
+    run('wget -O crane.yaml {}'.format(crane_file))
+    sudo('./crane lift')
+
+    sudo('docker ps')
+
+
+@task
+def update():
+    sudo('./crane kill tempus')
+    sudo('./crane lift tempus')
